7b70e67aa6df2e53dee8a9be7a38ff1cd8899d83
student <student@PC-16>
2018-11-12 18:45:26 +0100

    done

--- Factory Pattern 2/src/App.java
+++ Factory Pattern 2/src/App.java
@@ -7,9 +7,19 @@ public class App {
 		CarStore c2 = new CarStroreUK();
 		
 		c1.orderCar("SUV", "Black");
+		System.out.println(c1);
 		c2.orderCar("SUV", "Black");
+		System.out.println(c2);
 		c1.orderCar("Cabriolet", "Red");
+		System.out.println(c1);
 		c2.orderCar("Cabriolet", "Blue");
+		System.out.println(c2);
+		
+		
+		Car c3 = new CarStoreEU().orderCar("Sedan", "Yellow");
+		System.out.println(c3);
+		
+		
 		
 		
 
